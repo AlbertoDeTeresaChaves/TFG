05b1605104dac5503ac9ee64408890edde748f86
alberto <alberto.trabajo.es@gmail.com>
2024-05-01 14:15:27 +0200

    Agregando al Form Login el modo Reactivo

--- global-education/src/app/components/login/login.component.ts
+++ global-education/src/app/components/login/login.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { EmailValidator, FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
 
+  constructor(){}
+
+  loginForm = new FormGroup({
+    email : new FormControl('',[Validators.email, Validators.required]),
+    password : new FormControl('',Validators.required) 
+
+  }) 
+
+  onSubmit(){
+    console.log("HGola")
+  }
 }

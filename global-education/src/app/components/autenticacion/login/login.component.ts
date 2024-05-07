70b1b3f1be0a9aaabf5c014cad820eb27c9b69c7
alberto <alberto.trabajo.es@gmail.com>
2024-05-07 18:10:50 +0200

    Cambio de estilo del autentication

--- global-education/src/app/components/autenticacion/login/login.component.ts
+++ global-education/src/app/components/autenticacion/login/login.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { FormControl, FormGroupDirective, NgForm, ReactiveFormsModule, Validators } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatInputModule, MatFormField } from '@angular/material/input';
 import { RouterOutlet,RouterLink } from '@angular/router';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
+
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [ReactiveFormsModule,RouterOutlet,RouterLink],
+  imports: [ReactiveFormsModule,RouterOutlet,RouterLink,MatInputModule,MatFormField],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
@@ -12,11 +22,10 @@ export class LoginComponent {
 
   constructor(){}
 
-  loginForm = new FormGroup({
-    email : new FormControl('',[Validators.email, Validators.required]),
-    password : new FormControl('',Validators.required) 
+    emailFormControl = new FormControl('',[Validators.email, Validators.required]);
+    passwordFormControl = new FormControl('',Validators.required);
 
-  }) 
+    matcher = new MyErrorStateMatcher();
 
   onSubmit(){
     console.log("Hola")
--- global-education/src/app/components/autenticacion/register/register.component.ts
+++ global-education/src/app/components/autenticacion/register/register.component.ts
@@ -1,19 +1,30 @@
 import { Component } from '@angular/core';
-import { ReactiveFormsModule,FormControl, FormGroup, Validators } from '@angular/forms';
+import { ReactiveFormsModule,FormControl, Validators, FormGroupDirective, NgForm } from '@angular/forms';
+import { ErrorStateMatcher } from '@angular/material/core';
+import { MatInputModule, MatFormField } from '@angular/material/input';
+
+export class MyErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, MatFormField, MatInputModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  nameFormControl = new FormControl('',Validators.required);
+  emailFormControl = new FormControl('',[Validators.email, Validators.required]);
+  passwordFormControl = new FormControl('',Validators.required);
 
-  registerForm = new FormGroup({
-    name: new FormControl('',Validators.required),
-    email : new FormControl('',[Validators.email, Validators.required]),
-    password : new FormControl('',Validators.required) 
+  matcher = new MyErrorStateMatcher();
 
-  }) 
+onSubmit(){
+  console.log("Hola")
+}
 }
